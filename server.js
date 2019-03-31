167a909b9eef18bc27fd6ce69dd70b7e3152a2e6
Lisa Reinhardt <reinhal@gmail.com>
2019-03-31 18:46:09 -0500

    getting upload images to work

--- server.js
+++ server.js
@@ -1,34 +1,79 @@
 'use strict';
 
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const cloudinary = require('cloudinary');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
 const formData = require('express-form-data');
 const cors = require('cors');
 const { CLIENT_ORIGIN } = require('./config');
 
 const app = express();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+app.use(cors({ 
+  origin: CLIENT_ORIGIN 
+}))
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/')
+  }, 
+  filename: function(req, file, cb) {
+    console.log(file);
+    cb(null, file.originalname);
+  }
+}) 
+
+app.post('/image-upload', (req, res, next) => {
+  const upload = multer({ storage }).single('name-of-input-key')
+  console.log(req);
+  upload(req, res, function(err) {
+    if(err) {
+      return res.send(err)
+    }
+    console.log('file uploaded to server')
+    console.log(req.file)
+
+    cloudinary.config({ 
+      cloud_name: process.env.CLOUD_NAME, 
+      api_key: process.env.API_KEY, 
+      api_secret: process.env.API_SECRET
+    })
+
+    const path = req.file.path;
+    const uniqueFilename = new Date().toISOString();
+
+    cloudinary.uploader.upload(
+      path,
+      { public_id: `juskidin/${uniqueFilename}` },
+      function(err, image) {
+        if (err) return res.send(err);
+        console.log('file uploaded to Cloudinary')
+        // remove file from server
+        const fs = require('fs')
+        fs.unlinkSync(path)
+        res.json(image);
+      }
+    )
+  })
 });
 
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}));
 
-app.use(formData.parse);
 
-app.post('/image-upload', (req, res) => {
+  
+ 
 
-  const values = Object.values(req.files);
-  const promises = values.map(image => cloudinary.uploader.upload(image.path));
+// app.use(formData.parse())
 
-  Promise
-    .all(promises)
-    .then(results => res.json(results));
-});
+// app.post('/image-upload', (req, res) => {
 
-app.listen(process.env.PORT || 8080, () => console.log('Thumbs Up!'));
+//   const values = Object.values(req.files)
+//   const promises = values.map(uploadedImage => cloudinary.uploader.upload(uploadedImage.path))
+  
+//   Promise
+//     .all(promises)
+//     .then(results => res.json(results))
+//     .catch((err) => res.status(400).json(err))
+// })
+
+app.listen(process.env.PORT || 8080, () => console.log('👍'))
